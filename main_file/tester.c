176750661929947b4a670c6b42a36961f4e29dd1
Amirsamyar Mirzaei <ammirzae@c2r2p1.42vienna.com>
2026-04-20 13:16:05 +0200

    [V0.1] ft_isalpha is clean, made some adjustments to the whole repo.

---
+++ main_file/tester.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+    int i;
+
+    if (argc < 2)
+    {
+        printf("please provide an argument");
+        return (1);
+    }
+    i = 0;
+    while (argv[1][i] != '\0')
+    {
+        printf("%c = %d\n", argv[1][i], ft_isalpha(argv[1][i]));
+        i++;
+    }
+}
+
