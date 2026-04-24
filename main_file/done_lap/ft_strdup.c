8c66b83fc0d330cf9f282fc1ee49605739494810
Sony <ammirzae@student.42vienna.com>
2026-04-24 20:22:52 +0200

    [V0.3.8.9] removed all  unneede files, added strdup

---
+++ main_file/done_lap/ft_strdup.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *ft_strdup(char *src)
+{
+	int length;
+	char *copy;
+	int i;
+	
+	i = 0;
+//	length = ft_strlen(src);
+	length = strlen(src);
+	copy = malloc((length + 1) * sizeof(char));
+	if(copy == NULL)
+		return(NULL);
+	copy[length] = '\0';
+	while(src[i] != '\0')
+	{
+		copy[i] = src[i];
+		i++;
+	}
+	return(copy);
+}
+
+
+int	main(int argc, char **argv)
+{
+	if(argc != 2)
+	{
+		printf("please only add one argument");
+		return(1);
+	}
+
+
+	char *str;
+
+	str = ft_strdup(argv[1]);
+	printf("%s\n", str);
+	free(str);
+}
